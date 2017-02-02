6fe49021459518dd9472176f20a89d01d889e33c
Pascelle <pascelle.saintlaiurent@gmail.com>
2017-02-02 13:18:24 -0500

    argv.command chain, before DRY variables

--- app.js
+++ app.js
@@ -1,20 +1,50 @@
-console.log('Starting app'); 
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
 const notes = require('./notes.js');
 
-const argv = yargs.argv;
+const argv = yargs
+	//this is the setup for the add command (name, description, actions required)
+	.command('add', 'add a new note', {
+		title: {
+			describe: 'title of note',
+			//this describes what gets passed in for the title of the note
+			demand: true,
+			//the tells yargs whether or not this argument is required.  it is false by default
+			alias: 't'
+			//lets you provide a shortcut so you dont have to type --title, you can set it to a single character, like t.
+		},
+		body: {
+			describe: 'Body of note' ,
+			demand: true, 
+			alias: 'b'
+		}
+	})
+	.command('list', 'list all notes')
+	.command('read', 'read a note', {
+		title: {
+			describe: 'title of note',
+			demand: true,
+			alias: 't'
+		},
+	})
+	.command('remove', 'remove a note', {
+		title: {
+			describe: 'title of note',
+			demand: true,
+			alias: 't'
+		},
+	})
+	.help()
+	//we are making a help call.  this is a method so we're going to call it as a function and dont need to pass in any arguments.  the purpose of the help call is to set up yargs to return some useful info when someone runs the program 
+	.argv;
+//here we're calling command on yargs, and then argv on the return value for command
+//then we pass in three arguments to command (what the command is, what it does, an option object that lets us specify what arguments this command requires)
 //console.log(process.argv);
 //process object with argv (arguments vector) property which is an array of all of the command line arguments passed in
 //the whole point of this is so we can pass information into app.js via the command line (in real life this would be user input).  We can then grab the information passed in using process.argv and based on what was passed in do something with it.
 var command = argv._[0];
 //using the yarg module that yields an object with the commands etc.  the argv is the variable that holds the yargs.argv method that we can now use and we are grabbing the command that is entered in the command prompt which shows up in the object created by yarg in the 0 index place.
-
-
-console.log('command: ', command);
-console.log('Yargs', argv);
 
 if (command === 'add'){
 	var note = notes.addNote(argv.title, argv.body);
